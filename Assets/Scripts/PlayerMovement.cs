123d21d0e759d4568339de5636221ccfe96f71da
lmoody811 <70034954+lmoody811@users.noreply.github.com>
2020-12-01 16:44:37 -0600

    Added ability for character to move

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    //Tutorial found here: https://www.youtube.com/watch?v=Sqb-Ue7wpsI
+
+    private CharacterController character_Controller;
+
+    private Vector3 move_Direction;
+
+    public float speed = 5f;
+    private float gravity = 20f;
+
+    public float jump_Force = 10f;
+
+    private float vertical_Velocity;
+
+
+    void Awake()
+    {
+
+        character_Controller = GetComponent<CharacterController>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveThePlayer();
+    }
+
+    void MoveThePlayer()
+    {
+        move_Direction = new Vector3(Input.GetAxis(Axis.HORIZONTAL), 0f, Input.GetAxis(Axis.VERTICAL));
+
+        move_Direction = transform.TransformDirection(move_Direction);
+        move_Direction *= speed * Time.deltaTime;
+
+        character_Controller.Move(move_Direction);
+    }
+
+
+}
+
