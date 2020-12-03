195cf11771a11dd1c126f6aa7ccb954f85641ceb
lmoody811 <70034954+lmoody811@users.noreply.github.com>
2020-12-03 14:17:10 -0600

    Added weapons & ability to zoom

---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+
+    private WeaponManager weapon_Manager;
+
+    public float fireRate = 15f;
+
+    private float nextTimeToFire;
+    public float damage = 20f;
+
+    private Animator zoomCameraAnim;
+
+    private bool zoomed;
+
+    private Camera mainCam;
+
+    private GameObject crosshair;
+
+    private bool is_Aiming;
+
+
+    void Awake()
+    {
+        weapon_Manager = GetComponent<WeaponManager>();
+
+        Transform root = transform.Find("LookRoot");
+        zoomCameraAnim = root.GetComponentInChildren<Animator>();
+
+        crosshair = GameObject.FindWithTag(Tags.CROSSHAIR);
+
+        mainCam = Camera.main;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        WeaponShoot();
+        ZoomInAndOut();
+    }
+
+    void WeaponShoot()
+    {
+        if(weapon_Manager.GetCurrentSelectedWeapon().fireType == WeaponFireType.MULTIPLE)
+        {
+            if(Input.GetMouseButton(0) && Time.time > nextTimeToFire)
+            {
+                nextTimeToFire = Time.time + 1f / fireRate;
+
+                weapon_Manager.GetCurrentSelectedWeapon().ShootAnimation();
+            }
+        }
+        else
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+                if(weapon_Manager.GetCurrentSelectedWeapon().tag == Tags.AXE_TAG) {
+                    weapon_Manager.GetCurrentSelectedWeapon().ShootAnimation();
+                }
+
+                if(weapon_Manager.GetCurrentSelectedWeapon().bulletType == WeaponBulletType.BULLET)
+                {
+                    weapon_Manager.GetCurrentSelectedWeapon().ShootAnimation();
+
+                    //BulletFired();
+                }
+                
+            }
+        }
+    }
+
+    void ZoomInAndOut()
+    {
+        if (weapon_Manager.GetCurrentSelectedWeapon().weapon_Aim == WeaponAim.AIM)
+        {
+            if(Input.GetMouseButtonDown(1))
+            {
+                zoomCameraAnim.Play(AnimationTags.ZOOM_IN_ANIM);
+
+                crosshair.SetActive(false);
+            }
+            if(Input.GetMouseButtonUp(1))
+            {
+                zoomCameraAnim.Play(AnimationTags.ZOOM_OUT_ANIM);
+
+                crosshair.SetActive(true);
+            }
+
+        }
+
+        if(weapon_Manager.GetCurrentSelectedWeapon().weapon_Aim == WeaponAim.SELF_AIM)
+        {
+            if(Input.GetMouseButtonDown(1))
+            {
+                weapon_Manager.GetCurrentSelectedWeapon().Aim(true);
+                is_Aiming = true;
+
+            }
+            if (Input.GetMouseButtonUp(1))
+            {
+                weapon_Manager.GetCurrentSelectedWeapon().Aim(false);
+                is_Aiming = false;
+
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/WeaponManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponManager : MonoBehaviour
+{
+
+    [SerializeField]
+    private WeaponHandler[] weapons;
+
+    private int current_Weapon_Index;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        current_Weapon_Index = 0;
+        weapons[current_Weapon_Index].gameObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            TurnOnSelectedWeapon(0);
+        }
+
+        //add more of these if I add more weapons.. Alpha1 is 1 on the keyboard.. Alpha2 is 2 on the keyboard.. etc.
+    }
+
+    void TurnOnSelectedWeapon(int weaponIndex)
+    {
+        if(current_Weapon_Index == weaponIndex)
+        {
+            return;
+        }
+        weapons[current_Weapon_Index].gameObject.SetActive(false);
+
+        weapons[weaponIndex].gameObject.SetActive(true);
+
+        current_Weapon_Index = weaponIndex;
+    }
+
+    public WeaponHandler GetCurrentSelectedWeapon()
+    {
+        return weapons[current_Weapon_Index];
+    }
+}
