925bb8bddab36e65be636ea2b93c05d4d8af50d7
derrickcourtney <dcourtney@harding.edu>
2020-12-07 13:05:40 -0600

    Added functionality for menu
    The goal page currently has a placeholder, instead of a final description.

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    public TextMeshProUGUI title;
+    public TextMeshProUGUI goal;
+    public TextMeshProUGUI developers;
+    public GameObject startBTN;
+    public GameObject backBTN;
+    public GameObject goalBTN;
+    public GameObject quitBTN;
+
+    void Start()
+    {
+        showMenu();
+    }
+    // Start is called before the first frame update
+    public void startGame()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+
+    public void showInstructions()
+    {
+        backBTN.SetActive(true);
+        goal.enabled = true;
+
+        goalBTN.SetActive(false);
+        title.enabled = false;
+        developers.enabled = false;
+        startBTN.SetActive(false);
+        quitBTN.SetActive(false);
+    }
+
+    public void goBack()
+    {
+        showMenu();
+    }
+
+    void showMenu()
+    {
+        title.enabled = true;
+        developers.enabled = true;
+        startBTN.SetActive(true);
+        quitBTN.SetActive(true);
+        goalBTN.SetActive(true);
+
+        backBTN.SetActive(false);
+        goal.enabled = false;
+    }
+}
