c8553e927a52f96701b2adc09d57420a04e456be
derrickcourtney <dcourtney@harding.edu>
2020-12-11 09:40:33 -0600

    Added ability to jump between levels

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ApplyCheats();
+    }
+
+    void ApplyCheats()
+    {
+        goToLevel1();
+        goToLevel2();
+        goToLevel3();
+    }
+
+    void goToLevel1()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SceneManager.LoadScene("Level1");
+        }
+    }
+
+    void goToLevel2()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            SceneManager.LoadScene("Level2");
+        }
+    }
+
+    void goToLevel3()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            SceneManager.LoadScene("Level3");
+        }
+    }
+}
